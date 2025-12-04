1efc09c88f06ed850cfe8a76b50ed1f8e8cb1778
hupco <hupcikmatus08@gmail.com>
2025-12-04 21:34:02 +0100

    Day4 first part

--- AdventOfCode_4.cpp
+++ AdventOfCode_4.cpp
@@ -15,8 +15,8 @@ int main() {
     int freeRolls = 0;
 	vector<string> rows;
 	
-	unsigned int dy = [1, 1, 1, 0, -1, -1, -1, 0], 
-                 dx[-1, 0, 1, 1, 1, 0, -1, -1];
+	int dy[] = {1, 1, 1, 0, -1, -1, -1, 0},
+        dx[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 	
 	ifstream fin("day4.txt");
 	string temp;
@@ -24,15 +24,36 @@ int main() {
 		rows.push_back(temp);
 	}
 	
-	for(int = 0; i < rows.size(); i++) {
-	    int occupiedSpace = 0;
-	    if(i > 0 && row[i-1] == '.')
-	       
-	    
-	    if(occupiedSpace <= 4) {
-	        freeRolls += 1;
-        }
+	for(int y = 0; y < rows.size(); y++) {
+		for(int x = 0; x < rows[y].length(); x++) {
+			if(rows[y][x] != '@')
+				continue;
+				
+			int occupiedSpace = 0;
+		    
+		    for(int d = 0; d < 8; d++) {
+				if ((x == 0 && dx[d] == -1) || (y == 0 && dy[d] == -1) || 
+				    (x == rows[y].length() - 1 && dx[d] == 1) || (y == rows.size() - 1 && dy[d] == 1))
+				    continue;
+				
+				int checkPosY = y + dy[d],
+					checkPosX = x + dx[d];
+				
+		    	if(rows[checkPosY][checkPosX] == '@') {
+		    		occupiedSpace++;
+				}
+				if(occupiedSpace > 3) {
+					break;
+				}
+			}     
+		       
+		    if(occupiedSpace < 4) {
+		        freeRolls += 1;
+	        }
+		} 
     }
+    
+    cout << freeRolls << endl;
 	
 	return 0;
 }
